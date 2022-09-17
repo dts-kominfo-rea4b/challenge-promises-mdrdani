f745063a2d5a2d0417c91b7ca6285a585fee1ee1
Muhamad Dani Ramanda <45899199+mdrdani@users.noreply.github.com>
2022-09-17 22:08:43 +0700

    Challenge Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,31 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC } = require('./external.js');
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve) => {
+    const hasilBioskop = [];
+
+    promiseTheaterIXX().then((data) => {
+      hasilBioskop.push(...data);
+
+      promiseTheaterVGC().then((data) => {
+        hasilBioskop.push(...data);
+
+        if (emosi === 'marah') {
+          const marah = hasilBioskop.filter((item) => item.hasil === 'marah');
+
+          resolve(marah.length);
+        } else if (emosi === 'tidak marah') {
+          const tidakMarah = hasilBioskop.filter(
+            (item) => item.hasil === 'tidak marah'
+          );
+
+          resolve(tidakMarah.length);
+        }
+      });
+    });
+  });
+};
 
 module.exports = {
   promiseOutput,
